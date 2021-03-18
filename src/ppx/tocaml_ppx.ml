8de9b8d8767ec660fea80911aed816a44ee61a14
François Bobot <francois.bobot@cea.fr>
2021-03-18 22:21:06 +0100

    Add extension in expression
      a second pass is needed in order to get the transform the struct access field
      into a module access

--- src/ppx/tocaml_ppx.ml
+++ src/ppx/tocaml_ppx.ml
@@ -5,7 +5,7 @@ let imports = Queue.create ()
 
 let already_loaded = Hashtbl.create 10
 
-let get_filename ctxt path =
+let get_filename ~ctxt path =
   let source =
     ctxt |> Expansion_context.Extension.code_path |> Code_path.file_path
   in
@@ -40,7 +40,7 @@ let expand ~ctxt relative_filename args =
     | [ sha ] -> Some sha
     | _ -> Location.raise_errorf ~loc "imports \"PATH|URL\" \"sha\"?"
   in
-  let file = get_filename ctxt relative_filename in
+  let file = get_filename ~ctxt relative_filename in
   let id, sha = unique_file_name file in
   Option.iter
     (fun sha_expected ->
@@ -53,22 +53,72 @@ let expand ~ctxt relative_filename args =
     Hashtbl.add already_loaded id ();
     let str = open_file file in
     Queue.push (id, loc, str) imports);
-  Ast_builder.Default.pmod_ident ~loc { loc; txt = Lident id }
+  id
 
-let my_extension =
-  Extension.V3.declare "import" Extension.Context.module_expr
-    (* Ast_pattern.(single_expr_payload (elist (estring __))) *)
-    Ast_pattern.(
-      single_expr_payload
-        (alt
-           (map_result (estring __) ~f:(fun f -> f []))
-           (pexp_apply (estring __)
-              (many (pair (of_func (fun _ _ _ f -> f)) (estring __))))))
-    expand
+let payload_pattern () =
+  Ast_pattern.(
+    single_expr_payload
+      (alt
+         (map_result (estring __) ~f:(fun f -> f []))
+         (pexp_apply (estring __)
+            (many (pair (of_func (fun _ _ _ f -> f)) (estring __))))))
 
-let rule = Ppxlib.Context_free.Rule.extension my_extension
+let rules =
+  [
+    Ppxlib.Context_free.Rule.extension
+    @@ Extension.V3.declare "import" Extension.Context.module_expr
+         (payload_pattern ()) (fun ~ctxt file args ->
+           let id = expand ~ctxt file args in
+           let loc = Expansion_context.Extension.extension_point_loc ctxt in
+           Ast_builder.Default.pmod_ident ~loc { loc; txt = Lident id });
+    Ppxlib.Context_free.Rule.extension
+    @@ Extension.V3.declare "import" Extension.Context.expression
+         (payload_pattern ()) (fun ~ctxt file args ->
+           let id = expand ~ctxt file args in
+           let loc = Expansion_context.Extension.extension_point_loc ctxt in
+           Ast_builder.Default.(
+             pexp_extension ~loc
+               ( { loc; txt = "import_second_pass" },
+                 PStr
+                   [
+                     pstr_eval ~loc
+                       (pexp_ident ~loc { loc; txt = Lident id })
+                       [];
+                   ] )));
+  ]
+
+let convert_direct_access =
+  object
+    inherit Ast_traverse.map as super
+
+    val pattern =
+      Ast_pattern.(
+        pexp_field
+          (pexp_extension
+             (extension
+                (string "import_second_pass")
+                (single_expr_payload (pexp_ident __))))
+          __)
+
+    method! expression e =
+      let e = super#expression e in
+      Ast_pattern.parse pattern e.pexp_loc
+        ~on_error:(fun () -> e)
+        e
+        (fun payload lid ->
+          let rec add_top payload = function
+            | Lident field -> Ldot (payload, field)
+            | Ldot (lid, field) -> Ldot (add_top payload lid, field)
+            | Lapply _ -> assert false
+            (* not in a pexp_field *)
+          in
+          let lid = add_top payload lid in
+          let loc = e.pexp_loc in
+          Ast_builder.Default.pexp_ident ~loc { loc; txt = lid })
+  end
 
 let impl (stritem : structure) =
+  let stritem = convert_direct_access#structure stritem in
   let fold acc (id, loc, str) =
     Ast_builder.Default.(
       pstr_module ~loc
@@ -80,4 +130,4 @@ let impl (stritem : structure) =
   start @ stritem
 
 (** Note: enclose_impl can't be used because it is evaluated before the rule are run even if the result should be inserted after *)
-let () = Driver.register_transformation ~rules:[ rule ] ~impl "my_ext"
+let () = Driver.register_transformation ~rules ~impl "my_ext"
--- tests/run.t
+++ tests/run.t
@@ -29,3 +29,20 @@
   File "-", line 1, characters 11-41:
   Error: File $TESTCASE_ROOT/import.ml has a different digest "7f2f2c5002cb0a5c9643675b06d2fa72" than expected "error"
   [1]
+
+
+  $ tocaml - -impl  <<EOF
+  > print_string [% import "import.ml"].x;;
+  > EOF
+  module TOCAML_private_7f2f2c5002cb0a5c9643675b06d2fa72 =
+    struct let x = "helloworld" end
+  ;;print_string TOCAML_private_7f2f2c5002cb0a5c9643675b06d2fa72.x
+
+  $ tocaml - -impl  <<EOF
+  > print_string [% import "import.ml"].x;;
+  > print_string [% import "import.ml"].x;;
+  > EOF
+  module TOCAML_private_7f2f2c5002cb0a5c9643675b06d2fa72 =
+    struct let x = "helloworld" end
+  ;;print_string TOCAML_private_7f2f2c5002cb0a5c9643675b06d2fa72.x
+  ;;print_string TOCAML_private_7f2f2c5002cb0a5c9643675b06d2fa72.x
